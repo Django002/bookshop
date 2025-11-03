de20dd88a05eace9b5f4f03e0ea210fda50de6a5
Adam <adam_kadyrov_2001@mail.ru>
2025-11-03 16:35:13 +0300

    add carta

--- src/components/componentSection/sections/sectioTwo.jsx
+++ src/components/componentSection/sections/sectioTwo.jsx
@@ -13,7 +13,17 @@ function SectionTwo() {
                 </div>
             </div>
             <div className='SectionTwo_bocks'>
-                
+                <div className='cartra'>
+                    <div className='cartra_content'>
+                        <img src="" alt="" srcset="" />
+                        <span className='cartra_content'>Марсель Руссо</span>
+                        <h3 className='cartra_content'>Охота Амура</h3>
+                        <span className='cartra_content'>Холст, масло (50х80) </span>
+                        <span className='cartra_content'>14 500 руб</span>
+                        <button className='cartra_content' type="button"></button>
+                    </div>
+
+                </div>
             </div>
         </div>
         </>
