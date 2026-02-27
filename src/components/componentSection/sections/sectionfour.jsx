a39d5f30b1fb9688514a0ceee45aeef1bfd7ea6d
Adam Kadyrov <adam_kadyrov_2001@mail.ru>
2026-02-27 23:42:42 +0300

    fix

--- src/components/componentSection/sections/sectionfour.jsx
+++ src/components/componentSection/sections/sectionfour.jsx
@@ -1,16 +1,20 @@
 import './sectionfourstyle.css'
+import ramca from '../../../assets/Group210.png';
+import cartina1 from "../../../assets/Ellipse30.png";
+import cartina2 from "../../../assets/Ellipse31.jpg";
+import cartina3 from "../../../assets/Ellipse32.jpg";
 
 function Sectionfour() {
     return(
         <div className='Sectionfour'>
-            <img className='Sectionfourimg' src="src\assets\Group 210.png" alt="" srcset="" />
+            <img className='Sectionfourimg' src={ramca} alt="" srcset="" />
             <div className='Sectionfour_content'>
                 <h2>Наша команда</h2>
                 <p>Значимость этих проблем настолько очевидна, что базовый вектор развития позволяет оценить значение экспериментов, поражающих по своей масштабности и грандиозности. Мы вынуждены отталкиваться от того, что консультация с широким активом.</p>
                 <div className='Sectionfour_contentimg'>
-                    <img src="src\assets\Ellipse 30.png" alt="" />
-                    <img src="src\assets\Ellipse 31.jpg" alt="" />
-                    <img src="src\assets\Ellipse 32.jpg" alt="" />
+                    <img src={cartina1} alt="" />
+                    <img src={cartina2} alt="" />
+                    <img src={cartina3} alt="" />
                 </div>
             </div>
         </div>
